263bcd2728ffcc9704f26bd9be15258033a2f709
mxws <mxws@mxws.com>
2017-09-07 20:56:11 -0700

    Update gallowbot.py to rank commenters for gallowboob's 50 latest posts
    First major commit - logs into reddit using praw.ini file information, scrapes u/gallowboob's
    last 50 submissions. Steps through each submission to scrape all comments in a flattened
    structure. Steps through each comment, adding up user comments via use of a dict.
    
    Once finished, the script prints out the total number of comments and sorts the dict to show
    the 10 users who commented the most in the 50 submissions.
    
    TODO:
    Comment on the 10 users' last gallowboob comments, informing them of their ranking in gallowboob's
    fan club.

--- gallowbot.py
+++ gallowbot.py
@@ -1,6 +1,25 @@
 import praw
 
 r = praw.Reddit('bot1')
-r.login()
 
+counts = dict()
+comment_count = 0
+
+for s in r.redditor('gallowboob').submissions.new(limit=50):
+    s.comments.replace_more(limit=0)
+    for comment in s.comments.list():
+        if not comment.author:
+            continue
+        comment_count += 1
+        fan = comment.author.name
+        if fan in counts:
+            counts[fan] += 1
+        else:
+            counts[fan] = 1
+
+sorted_val = sorted(counts.items(), key=lambda x:x[1], reverse=True)
+
+print("Comments = {0}".format(comment_count))
+for x in sorted_val[:10]:
+    print(x[0], x[1])
 
